31b4bb2f52e2a8211a7425c333ecb57212fb5409
LeonardoRebello <31066683+LeonardoRebello@users.noreply.github.com>
2019-09-13 11:07:14 -0300

    ~

--- Programas_Aula05/LOW-PASS_WINDOWED-SINC_FILTER.py
+++ Programas_Aula05/LOW-PASS_WINDOWED-SINC_FILTER.py
@@ -1,17 +1,17 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-Vetor_in = []
-Vetor_out = []
+FC = float(input("FC: "))
+BW = float(input("BW: "))
+
+M = 4 / BW
+
 Filter_kernel = []
-
-FC = .14
-M = 100
-
 for i in range(100):
     if i - M/2 == 0:
-        Filter_kernel[i] = 2 * np.pi * FC
+        Filter_kernel.append(2 * np.pi * FC)
     else:
-        Filter_kernel[i] = np.sin(2 * np.pi * FC * (i - M/2)) / (i - M/2)
+        Filter_kernel.append(np.sin(2 * np.pi * FC * (i - M/2)) / (i - M/2))
     Filter_kernel[i] = Filter_kernel[i] * (0.54 - 0.46 * np.cos(2 * np.pi * i / M))
 
 SUM = 0
@@ -21,8 +21,7 @@ for i in range(100):
 for i in range(100):
     Filter_kernel[i] = Filter_kernel[i] / SUM
 
-for i in range(100, 4999):
-    Vetor_out[i] = 0
-    for j in range(100):
-        Vetor_out[i] = Vetor_out[i] + Vetor_in[i - j] * Filter_kernel[j]
+xAxis = np.linspace(0, len(Filter_kernel), len(Filter_kernel), endpoint=False)
 
+plt.plot(xAxis, Filter_kernel)
+plt.show()
